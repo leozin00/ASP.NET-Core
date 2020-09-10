9dc105ad993d8b1b0f4655bc1d1ad253e934b3a4
Leonardo <leo.vasconcelos2000@gmail.com>
2020-09-10 15:23:05 -0300

    Adicionado Controller, Model e View ao projeto.

---
+++ LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+
+            Produto produto = GetProduto();
+
+            return View(produto);
+            //return new ContentResult() { Content = "Produto -> Visualizar", ContentType="text/html" };
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto(){
+                Id = 1,
+                Nome = "Xbox",
+                Descricao = "Legal",
+                Valor = 2000.00M
+            };
+        } 
+    }
+}
